7f8014142bfbc736587d1004193296f906fddf34
Nathan <nathan.wally.anderson@gmail.com>
2025-11-07 00:04:11 -0600

    finalizing simulations. began working through moments docs

--- validation/fwdpy/paramMaker.R
+++ validation/fwdpy/paramMaker.R
@@ -12,3 +12,18 @@ for(s in c(1e-3,5e-3,1e-2)){
 master$seed <- sample(1:1e4,nrow(master))
 write.table(master, file = "params.txt",
             row.names = F,col.names = F, quote=F)
+
+library(dplyr)
+master <- data.frame()
+for(s in c(1e-3,5e-3,1e-2)){
+  for(n in c(1e3,5e3,1e4)){
+    for(rep in 1:1000){
+      master <- dplyr::bind_rows(master,
+                                 data.frame(s,
+                                            n))
+    }
+  }
+}
+master$seed <- sample(1:1e5,nrow(master))
+write.table(master, file = "morereps.txt",
+            row.names = F,col.names = F, quote=F)
--- validation/fwdpy/sfsFigureMaker.R
+++ validation/fwdpy/sfsFigureMaker.R
@@ -5,6 +5,43 @@ setwd("~/Documents/GitHub/BGSdemo/validation/fwdpy")
 params <- fread("params.txt")
 names(params) <- c("s","N","seed")
 
+
+setwd("/media/nathan/T7/BGSdemo/EquilData")
+master <- data.table()
+for(file in list.files()){
+  par <- params %>% filter(paste(seed,".csv",sep="")==file)
+  df <- fread(file) %>% as.matrix()
+  df <- df[,1]
+  master <- dplyr::bind_rows(master,data.table(s = par$s,
+                                               N = par$N,
+                                               seed = par$seed,
+                                               frq = df))
+}
+
+# ggplot(master) + 
+#   geom_density(aes(frq, color = as.factor(s))) + 
+#   facet_wrap(vars(N)) +
+#   scale_y_log10() +
+#   scale_x_log10()
+
+dens <- master %>% group_by(N,s) %>% 
+  reframe(densx = density(frq, 
+                          from =1/(2*unique(N)), 
+                          to = 1 - 1/(2*unique(N)))$x,
+          densy = density(frq, 
+                          from =1/(2*unique(N)), 
+                          to = 1 - 1/(2*unique(N)))$y,
+          mx = max(densy)) %>%
+  mutate(densy = densy / mx) %>% 
+  ungroup()
+
+ggplot(dens) + 
+  geom_line(aes(x = densx, 
+                y = densy, 
+                color = as.factor(s))) +
+  facet_wrap(vars(N)) +
+  scale_y_log10() 
+
 setwd("/media/nathan/T7/BGSdemo/genMats")
 master <- data.table()
 for(file in list.files()){
